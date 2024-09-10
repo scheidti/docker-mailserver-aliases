25887174731439a80ebab0ef486ea4e29f65e945
Christian Scheid <christian@scheid.tech>
2024-09-10 21:14:37 +0200

    use alias and domain on add alias submit

--- frontend/src/lib/AddAlias.svelte
+++ frontend/src/lib/AddAlias.svelte
@@ -39,7 +39,7 @@
 				headers: {
 					"Content-Type": "application/json",
 				},
-				body: JSON.stringify({ alias, email }),
+				body: JSON.stringify({ alias: aliasAndDomain, email }),
 			});
 
 			if (response.status === 201) {
